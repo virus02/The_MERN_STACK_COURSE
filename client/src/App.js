29525b279f4410b30df1343ab1d9d2c3bbd99aa4
sharathnayak <sharath.n@bolt360.io>
2021-03-17 12:57:51 +0530

    client setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppNavbar from './components/AppNavbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
