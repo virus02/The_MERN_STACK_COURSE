e5a56813a0d031b81a03c501f42d6e8cb7b82703
sharathnayak <sharath.n@bolt360.io>
2021-04-29 13:18:56 +0530

    finished redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoopingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -12,7 +14,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoopingList />
+        <Container>
+          <ItemModal />
+          <ShoopingList />
+        </Container>
       </div>
     </Provider>
   );
